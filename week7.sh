2afbfa6b6803dc5afbbd424e0590735df104bb74
Charis Davis <davischaris2@gmail.com>
2018-10-09 20:57:25 -0700

    Week 7 assignment for Dna Sequences

---
+++ week7.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+#For the different bases in the DNA sequences calculate the frequencies.
+
+dnaSeq = input ("Enter your DNA sequence:")
+
+#Begin calculating the entire length of the given sequence.
+print ("Total Sequence Length:")
+print (len(dnaSeq))
+
+#count total number of A's.
+print ("Number of As:")
+print (dnaSeq.count ('a'))
+
+#count total number of C's.
+print ("Number of Cs:")
+print (dnaSeq.count ('c'))
+
+#count total number of G's.
+print ("Number of Gs:")
+print (dnaSeq.count ('g'))
+
+#count total number of T's.
+print ("Number of Ts:")
+print (dnaSeq.count ('t'))
