fd6c1edac40cd64dd8a380d72cc701f5c70739ba
jembrown <jeremymbrown@gmail.com>
2018-12-08 16:02:35 -0600

    Comments added

--- week7.py
+++ week7.py
@@ -10,16 +10,20 @@ print (len(dnaSeq))
 
 #count total number of A's.
 print ("Number of As:")
-print (dnaSeq.count ('a'))
+print (dnaSeq.lower().count ('a'))
 
 #count total number of C's.
 print ("Number of Cs:")
-print (dnaSeq.count ('c'))
+print (dnaSeq.lower().count ('c'))
 
 #count total number of G's.
 print ("Number of Gs:")
-print (dnaSeq.count ('g'))
+print (dnaSeq.lower().count ('g'))
 
 #count total number of T's.
 print ("Number of Ts:")
-print (dnaSeq.count ('t'))
+print (dnaSeq.lower().count ('t'))
+
+# DB: Good, although this won't work properly if the sequence is upper case. You can
+#     avoid this by adding .lower() after dnaSeq (see above). Also, it's a good idea to 
+#     only use .sh for shell scripts. Instead, use .py for python.
